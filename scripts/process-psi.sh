c32d3f6deb6b625aa2bbd1fffba6e1ad8962204e
mircare <mircare@users.noreply.github.com>
2019-01-22 18:47:56 -0800

    added support to MacOS

--- scripts/process-psi.sh
+++ scripts/process-psi.sh
@@ -4,8 +4,8 @@
 # the number of alignments and the sequences in the following lines
 
 
-ff=`echo $1 | sed 's/\.psi/\.flatpsi/g'`
+ff=`echo $1 | sed -e 's/\.psi/\.flatpsi/g'`
 wc -l $1 |awk '{print $1}' > $ff
 awk '{print $2}' $1 >> $ff
-sed 's/-/./g' $ff -i
-sed '2s/\.//g' $ff -i
+sed -i'' -e 's/-/./g' $ff
+sed -i'' -e '2s/\.//g' $ff
